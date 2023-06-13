c6cb920118f2389f637e741a855a8faa014a6d13
hhy <hhy@qq.com>
2023-06-13 20:05:22 +0800

    dev修改

--- local_test/src/main/java/com/example/Controller/HelloController.java
+++ local_test/src/main/java/com/example/Controller/HelloController.java
@@ -3,5 +3,6 @@ package com.example.Controller;
 public class HelloController {
     public static void main(String[] args) {
         System.out.println("本地创建");
+        System.out.println("dev创建");
     }
 }
