f85b1f2b9f0fb1939b68371e7e4cd0e166cecd4c
RoninHh <136295291+RoninHh@users.noreply.github.com>
2023-06-13 19:51:58 +0800

    Update HelloController.java
    Ronin修改HelloController

--- local_test/src/test/java/com/example/local_test/Conntroller/HelloController.java
+++ local_test/src/test/java/com/example/local_test/Conntroller/HelloController.java
@@ -3,5 +3,6 @@ package com.example.local_test.Conntroller;
 public class HelloController {
     public static void main(String[] args) {
         System.out.println("Hello Github!");
+        System.out.println("Ronin创建");
     }
 }
